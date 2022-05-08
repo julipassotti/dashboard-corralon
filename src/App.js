127ecfc4cdd937a09c9727e10bf1f89afee0ac6c
julipassotti <julietapassotti@gmail.com>
2022-05-08 18:59:45 -0300

    configurando Home para mostrar estadisticas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.png';
+import logo from './Logo-corralon.png';
 import './App.css';
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
--- src/components/Products.js
+++ src/components/Products.js
@@ -15,36 +15,35 @@ function Products() {
         fetchApi();
         }, []
     )
-
-    console.log(allProducts)
-    console.log(setAllProducts)
-
+    
     // useEffect( () => {
     //     console.log("Se actualizo el componente")
     //     fetchApi();
     //     }, [products]
     // )
-    
+
     return(
         <>
-            <h2>Productos</h2>
-            <ul>
-                { allProducts.length === 0 && <p>No hay productos</p> }
-                {
-                    allProducts.map((product, i) => {
-                        return (
-                            <li key={i}>
-                                <h3>{product.name}</h3>
-                                <Link to={`/products/${ product.id }`}>Ver Detalle</Link>
-                                {/* <p><b>Descripcion: </b>{product.description}</p>
-                                <p><b>Precio: </b>{product.price}</p>
-                                <p><b>Stock: </b>{product.stock}</p>
-                                <p><b>Categoria: </b>{product.category.name}</p> */}
-                            </li>
-                        )
-                    })
-                }
-            </ul>
+            <h2>Nuestros Productos</h2>
+            <section>
+                <ul>
+                    { allProducts.length === 0 && <p>No hay productos</p> }
+                    {
+                        allProducts.map((product, i) => {
+                            return (
+                                <li key={i}>
+                                    <h3>{product.name}</h3>
+                                    <Link to={`/products/${ product.id }`}>Ver Detalle</Link>
+                                    {/* <p><b>Descripcion: </b>{product.description}</p>
+                                    <p><b>Precio: </b>{product.price}</p>
+                                    <p><b>Stock: </b>{product.stock}</p>
+                                    <p><b>Categoria: </b>{product.category.name}</p> */}
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </section>
         </>
     )
 }
